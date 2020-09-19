b2cccad761df69f6b982389915f96c12c16ded1b
Gabriel <gabi_borges01@hotmail.com>
2020-09-19 15:45:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,69 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const path = require('path');
+const path = require("path");
 
-const ObjectId = require('mongodb').ObjectID;
+const ObjectId = require("mongodb").ObjectID;
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://desafio1cdm:desafio1cdm@desafio1cdm.njqir.mongodb.net/Desafio1CDM";
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://desafio1cdm:desafio1cdm@desafio1cdm.njqir.mongodb.net/Desafio1CDM";
 
 MongoClient.connect(uri, (err, client) => {
-    if (err) return console.log(err);
-    db = client.db('Desafio1CDM');
+  if (err) return console.log(err);
+  db = client.db("Desafio1CDM");
 
-    app.listen(3000, () => {
-        console.log('Server running on port 3000');
-    });
+  app.listen(3000, () => {
+    console.log("Server running on port 3000");
+  });
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/', (req, res) => {
-    let cursor = db.collection('data').find();
+app.get("/", (req, res) => {
+  let cursor = db.collection("data").find();
 });
 
-app.get('/show', (req, res) => {
-    db.collection('data').find().toArray((err, results) => {
-        if (err) return console.log(err);
-        res.render('show.ejs', { data: results });
-    });
-}); 
-
-app.post('/show', (req, res) => {
-    db.collection('data').save(req.body, (err, result) => {
-        if (err) return console.log(err);
-
-        console.log('Save in DB');
-        res.redirect('/show');
+app.get("/show", (req, res) => {
+  db.collection("data")
+    .find()
+    .toArray((err, results) => {
+      if (err) return console.log(err);
+      res.render("show.ejs", { data: results });
     });
 });
 
-app.route('/edit/:id').get((req, res) => {
+app.post("/show", (req, res) => {
+  db.collection("data").save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log("Save in DB");
+    res.redirect("/show");
+  });
+});
+
+app
+  .route("/edit/:id")
+  .get((req, res) => {
     let id = req.params.id;
 
-    db.collection('data').find(ObjectId(id)).toArray((err, result) => {
+    db.collection("data")
+      .find(ObjectId(id))
+      .toArray((err, result) => {
         if (err) return res.send(err);
-        res.render('edit.ejs', { data: result });
-    });
-}).post((req, res) => {
+        res.render("edit.ejs", { data: result });
+      });
+  })
+  .post((req, res) => {
     let id = req.params.id;
     let nome = req.body.nome;
     let sobrenome = req.body.sobrenome;
@@ -68,32 +76,36 @@ app.route('/edit/:id').get((req, res) => {
     let estado = req.body.estado;
     let cidade = req.body.cidade;
 
-    db.collection('data').updateOne({_id: ObjectId(id)}, {
+    db.collection("data").updateOne(
+      { _id: ObjectId(id) },
+      {
         $set: {
-            nome: nome,
-            sobrenome: sobrenome,
-            email: email,
-            contato1: contato1,
-            contato2: contato2,
-            endereco: endereco,
-            complemento: complemento,
-            cep: cep,
-            estado: estado,
-            cidade: cidade
-        }
-    }, (err, result) => {
+          nome: nome,
+          sobrenome: sobrenome,
+          email: email,
+          contato1: contato1,
+          contato2: contato2,
+          endereco: endereco,
+          complemento: complemento,
+          cep: cep,
+          estado: estado,
+          cidade: cidade,
+        },
+      },
+      (err, result) => {
         if (err) return res.send(err);
-        res.redirect('/show');
-        console.log('Updated in DB!');
-    });
-});
+        res.redirect("/show");
+        console.log("Updated in DB!");
+      }
+    );
+  });
 
-app.route('/delete/:id').get((req, res) => {
-    let id = req.params.id;
+app.route("/delete/:id").get((req, res) => {
+  let id = req.params.id;
 
-    db.collection('data').deleteOne({_id: ObjectId(id)}, (err, result) => {
-        if (err) return res.send(500, err);
-        console.log('Deleted in DB!');
-        res.redirect('/show');
-    });
+  db.collection("data").deleteOne({ _id: ObjectId(id) }, (err, result) => {
+    if (err) return res.send(500, err);
+    console.log("Deleted in DB!");
+    res.redirect("/show");
+  });
 });
