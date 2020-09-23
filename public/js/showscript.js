50d81599cbabd01f071ac8bf9939efa6497b7041
Gabriel <gabi_borges01@hotmail.com>
2020-09-22 23:34:43 -0300

    Testes com JQuery

--- public/js/showscript.js
+++ public/js/showscript.js
@@ -1,22 +1,22 @@
 // Filter
-function myFunction() {
+function filterFunction() {
   let input, filter, table, tr, td, i, txtValue;
 
-  input = document.getElementById("myInput");
+  input = document.getElementById('myInput');
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
+  table = document.getElementById('myTable');
+  tr = table.getElementsByTagName('tr');
 
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName('td')[0];
 
     if (td) {
       txtValue = td.textContent || td.innerText;
 
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
+        tr[i].style.display = '';
       } else {
-        tr[i].style.display = "none";
+        tr[i].style.display = 'none';
       }
     }
   }
