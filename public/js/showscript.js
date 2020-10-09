14c47f32211b65954f40756009cc5a1ae80f10e3
Gabriel <gabi_borges01@hotmail.com>
2020-10-08 23:08:36 -0300

    Adicionando máscara no index

--- public/js/showscript.js
+++ public/js/showscript.js
@@ -26,21 +26,28 @@ function filterFunction() {
 // Máscara de contato
 function contatoMask() {
   let contato1 = document.getElementById('contato1');
+  let er = /[^0-9-./]/;
+  er.lastIndex = 0;
   
-  if (contato1.value.length === 2) {
-    contato1.value += " ";
-  } else if (contato1.value.length === 8 || contato1.value.length === 9) {
+  if (contato1.value.length === 7 || contato1.value.length === 8) {
     contato1.value += "-";
+  } else if (er.test(contato1.value)) {
+    contato1.value = "";
   }
 }
+
 
 // Máscara de CEP
 function cepMask() {
   let cep = document.getElementById('cep');
+  let er = /[^0-9-./]/;
+  er.lastIndex = 0;
 
   if (cep.value.length === 2) {
     cep.value += ".";
   } else if (cep.value.length === 6) {
     cep.value += "-";
+  } else if (er.test(cep.value)) {
+    cep.value = "";
   }
 }
