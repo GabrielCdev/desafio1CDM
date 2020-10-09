e8fa180d370456c2d23ae5ed1e8dbce44e76d965
Jeferson Lucas Tavares <jefersonsaj_tavares@hotmail.com>
2020-10-09 20:30:05 -0300

    Mascaras index e edit consertadas Jeff

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -1,7 +1,4 @@
-const { when } = require("jquery");
-const { MaxKey } = require("mongodb");
-
 $(document).ready(() => {
-    $('#contato').mask('(00) 00000-0000');
+    $('#InpTel').mask('(00) 00000-0000');
     $('#inpCnpj').mask('00.0000.00/0000-00');
   })
