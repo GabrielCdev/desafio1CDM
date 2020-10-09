911cf7b9b9974ce91b3612ba87d6e64f6d8dc767
Jeferson Lucas Tavares <jefersonsaj_tavares@hotmail.com>
2020-10-09 18:13:13 -0300

    Jeff atualizações views, js, css

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -1,25 +1,7 @@
+const { when } = require("jquery");
 const { MaxKey } = require("mongodb");
 
-function cnpjFunc() {
-
-    let inpCnpj = document.getElementById('inpCnpj');
-
-    if (inpCnpj.value.length == 2 || inpCnpj.value.length == 7) {
-        inpCnpj.value += ".";
-    } else if (inpCnpj.value.length === 10) {
-        inpCnpj.value += "/";
-    } else if (inpCnpj.value.length == 15) {
-        inpCnpj.value += "-";
-    }
-
-}
-
-function contatoFunc() {
-    let contato = document.getElementById('contato');
-
-    if (contato.value.length == 2) {
-        contato.value += " ";
-    } else if (contato.value.length == 4 || contato.value.length == 9) {
-        contato.value += "-";
-    }
-}
+$(document).ready(() => {
+    $('#contato').mask('(00) 00000-0000');
+    $('#inpCnpj').mask('00.0000.00/0000-00');
+  })
